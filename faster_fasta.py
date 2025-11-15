38a090cb882ef88ce075ba324fa7bc6e1cb0cf39
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2025-11-15 19:16:56 +0000

    Chore: Python configuration files

--- faster_fasta.py
+++ faster_fasta.py
@@ -1,5 +1,5 @@
-# Faster FASTA
-
+#!/usr/bin/env python3
+"""
 Faster FASTA is a collection of command-line utilities for processing memory-mapped plain-text FASTA files.
 It leverages StringZilla to provide high-performance functionality for:
 
@@ -12,3 +12,15 @@ It leverages StringZilla to provide high-performance functionality for:
 - transliterating from DNA to RNA via `fasta-dna2rna`
 - format line wrapping via `fasta-wrap`
 - sample sequences randomly via `fasta-sample`
+"""
+
+import stringzilla as sz
+
+class FASTA:
+    
+    def __init__(self, filepath):
+        self._filepath = filepath
+        self._file = sz.File(filepath)
+        self._str = sz.Str(self._file)
+        
+    
